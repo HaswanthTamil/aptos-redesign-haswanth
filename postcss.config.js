744a270c5ff2a828f0667686bdf36880d9995069
ghost <ghost@devmail.com>
2025-06-29 13:56:26 +0530

    Clean up config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [require("@tailwindcss/postcss")(), require("autoprefixer")],
+  plugins: [
+    "@tailwindcss/postcss", // ✅ string! Not require()
+    "autoprefixer",
+  ],
 }
